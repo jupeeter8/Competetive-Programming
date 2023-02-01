4b275f27db155387af7fd2be19c608befe1ea16a
Anirudh <aswalanirudh@outlook.in>
2023-02-01 11:33:25 +0530

    Practice: Dutch nation flag using 3 pointers

--- Practice/Arrays/DutchFlagProblem.py
+++ Practice/Arrays/DutchFlagProblem.py
@@ -25,14 +25,33 @@ class Solution:
 
         return array
 
+    def threePointer(self, array: List[int], n: int) -> List[int]:
+        mid: int = 0
+        lo: int = 0
+        hi: int = n - 1
+
+        while mid <= hi:
+            if array[mid] == 1:
+                mid = mid + 1
+
+            elif array[mid] == 0:
+                array[mid], array[lo] = array[lo], array[mid]
+                lo = lo + 1
+                mid = mid + 1
+
+            elif array[mid] == 2:
+                array[mid], array[hi] = array[hi], array[mid]
+                hi = hi - 1
+        return array
+
 
 def main():
     sol = Solution()
 
-    n = 7
-    array = [1, 2, 0, 1, 0, 0, 2]
+    n = 9
+    array = [0, 1, 1, 2, 0, 1, 0, 0, 2]
 
-    print(sol.sort012(array, n))
+    print(sol.threePointer(array, n))
 
 
 main()
