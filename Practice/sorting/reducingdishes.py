18d1a8cecec84e62163766213da6e5799935e14b
Anirudh <aswalanirudh@outlook.in>
2023-03-30 22:56:21 +0530

    Practice Reducing Dishes

--- Practice/sorting/reducingdishes.py
+++ Practice/sorting/reducingdishes.py
@@ -1,15 +1,33 @@
+import random
+
+
 def reduceDishes(sat: list[int]) -> int:
+    # Fucntion Checks for total satisfaction using every suffix
     sat.sort()
-    p = 0
-    c = 0
-    r = 0
+    satisfaction = 0
+    for i in range(len(sat)):
+        mul = 1
+        curr_satisfaction = 0
+        for j in range(i, len(sat)):
+            curr_satisfaction = curr_satisfaction + sat[j] * mul
+            mul += 1
+        satisfaction = max(curr_satisfaction, satisfaction)
+    return satisfaction
+
+
+def reduceDishes2(sat: list[int]) -> int:
+    sat.sort()
+    satisfaction = 0
+    total_satisfation = 0
+    prefix_sum = 0
 
     for i in reversed(range(len(sat))):
-        p += sat[i]
-        c += p
-        r = max(r, c)
-    return r
+        prefix_sum += sat[i]
+        total_satisfation += prefix_sum
+        satisfaction = max(satisfaction, total_satisfation)
+    return satisfaction
 
 
-sat = [-1, -8, 0, 5, -7]
-reduceDishes(sat)
+sat = [random.randint(-100, 100) for i in range(10000)]
+# print(reduceDishes(sat))
+print(reduceDishes2(sat))
