8cba00e61cd7fcf51f0f4c7c73af8ef1c523a63a
Anirudh <aswalanirudh@outlook.in>
2023-04-20 15:21:14 +0530

    Practice: TOH

---
+++ Practice/DP Recursion/toh.py
@@ -0,0 +1,12 @@
+def toh(source: str, dest: str, aux: str, rings: int) -> None:
+    if rings == 1:
+        pass
+        # print(f"Move disk {rings} from {source} to {dest}")
+
+    else:
+        toh(source, aux, dest, rings - 1)
+        # print(f"Move disk {rings} from {source} to {dest}")
+        toh(aux, dest, source, rings - 1)
+
+
+toh("A", "C", "B", int(input()))
